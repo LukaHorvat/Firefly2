0492472da57279741a3dd03f3cb279482663bbfa
Luka Horvat <lukahorvat9@gmail.com>
2013-11-19 16:30:18 +0100

    Started work on templating

--- BuildProject/Program.cs
+++ BuildProject/Program.cs
@@ -17,6 +17,15 @@ namespace BuildProject
 {
 	class Program
 	{
+		[EntityClass("NormalShape",
+			new IncludeComponent<RenderBufferComponent>(),
+			new IncludeComponent<TransformComponent>(),
+			new IncludeComponent<GeometryComponent>(),
+			new IncludeComponent<ShapeColorComponent>(),
+			new IncludeComponent<TreeNodeComponent>()
+		)]
+		Entity ent;
+
 		static void Main(string[] args)
 		{
 			var stage = new Stage(800, 500, "Hello World");
@@ -37,6 +46,9 @@ namespace BuildProject
 			circle.GetComponent<GeometryComponent>().Add(new Vector2d(0, 0));
 			circle.GetComponent<ShapeColorComponent>().Add(Color.HSVToRGB(0, 0, 0, 1));
 			stage.TreeNode.AddChild(circle);
+
+			Console.WriteLine(EntityGenerator.GenerateClasses());
+			VisualStudioHelper
 
 			stage.Run();
 		}
---
+++ Firefly2/Templating/Attributes/IncludeComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firefly2.Templating.Attributes
+{
+	public abstract class IncludeComponent : Attribute
+	{
+		public Type ComponentType;
+		public IncludeComponent(Type componentType)
+		{
+			ComponentType = componentType;
+		}
+	}
+}
